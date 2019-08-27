5a74746c77cee4962f1210ccec5605fc0a31406a
Hans Garcia <hansgarcia96@gmail.com>
2019-08-26 20:54:13 -0400

    countdown for upcoming rocket and spacex and elon's twitter on the landing page

--- routes/spacex-routes.js
+++ routes/spacex-routes.js
@@ -1,17 +1,37 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const moment = require("moment");
 
-// SPACEX API 
+// SPACEX API
 const SpacexApiWrapper = require("spacex-api-wrapper");
 
-SpacexApiWrapper.info().then(function(data) {
-  console.log(data);
-});
+// SpacexApiWrapper.info().then(function(data) {
+//   console.log(data);
+// });
 
 SpacexApiWrapper.getNextLaunch().then(function(data) {
-  console.log(data);
-  
+  let launchDate = data.launch_date_local;
+console.log("the launchdate is " + launchDate)
+
+// SpacexApiWrapper.getAllRockets().then(function(data) {
+//   let rocketName = data.rocket_name
+//   console.log(rocketName)
+// })
+
+
+// COUNTDOWN 
+  let now = moment().format();
+  console.log("now is " + now);
+
+  var theDay = moment().format(); 
+  console.log("the day is " + theDay)
+
+  let fromNow = moment(launchDate).endOf(launchDate).fromNow()
+
+  console.log(`The moment starts ` + fromNow)
+
 });
+
 
 module.exports = router;
