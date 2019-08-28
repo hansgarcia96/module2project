06efc68242db79a8f3d808f80d3b144023927088
Hans Garcia <hansgarcia96@gmail.com>
2019-08-27 23:17:02 -0400

    new countdown

--- routes/spacex-routes.js
+++ routes/spacex-routes.js
@@ -14,11 +14,75 @@ SpacexApiWrapper.getNextLaunch().then(function(data) {
   let launchDate = data.launch_date_local;
   console.log(launchDate)
 
+
+
+
+
   // UNIX TIME
   let launchDateUnix = data.launch_date_unix;
     // console.log(moment(launchDateUnix));
   let launchDateUTC = data.launch_date_utc;
-    console.log(moment(launchDateUTC).format())
+    
+  perfectLaunchDate = moment(launchDateUTC).format("MMM D, YYYY HH:mm:ss")
+
+  console.log(perfectLaunchDate)
+
+
+  // JS Countdown from W3
+
+// Set the date we're counting down to
+var countDownDate = new Date(perfectLaunchDate).getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  console.log( days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ");
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    console.log("EXPIRED");
+  }
+}, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // console.log("the launchdate is " + launchDate)
